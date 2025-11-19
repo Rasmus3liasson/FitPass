f9056eb756e8734a2ca19b14b287313e5f9f5648
Rasmus3liasson <ras.muse@hotmail.com>
2025-11-19 19:59:19 +0100

    remopved unessasary log

--- backend/src/routes/stripe.ts
+++ backend/src/routes/stripe.ts
@@ -1,6 +1,3 @@
-// Modular Stripe routes - refactored for better separation of concerns
-// Original monolithic file backed up as stripe.ts.backup
-
 import stripeRouter from "./stripe/index";
 
 export default stripeRouter;
